50a1adb940238054e3fd3a581c8250a946ad186e
Tamas Rev <tamas.rev@gmail.com>
2016-07-04 23:26:03 +0200

    more generic way to create tuples

--- Tuples/src/com/tamasrev/tuples/Tup.java
+++ Tuples/src/com/tamasrev/tuples/Tup.java
@@ -5,27 +5,26 @@ import java.lang.reflect.Proxy;
 public class Tup {
 
 	public static <T1, T2> Tuple2<T1, T2> tup2() {
-		TupleInvocationHandler tih = new TupleInvocationHandler(2);
-		return createProxy2(tih);
+		return Tup.<Tuple2<T1, T2>>createProxy(Tuple2.class, 2);
 	}
 
 	public static <T1, T2> Tuple2<T1, T2> tup(T1 v1, T2 v2) {
-		TupleInvocationHandler tih = new TupleInvocationHandler(v1, v2);
-		return createProxy2(tih);
+		return createProxy(Tuple2.class, v1, v2);
 	}
 
 	public static <T1, T2, T3> Tuple3<T1, T2, T3> tup3() {
-		TupleInvocationHandler tih = new TupleInvocationHandler(3);
-		return createProxy3(tih);
+		return Tup.<Tuple3<T1, T2, T3>>createProxy(Tuple3.class, 3);
 	}
 	
 	@SuppressWarnings("unchecked")
-	private static <T1, T2> Tuple2<T1, T2> createProxy2(TupleInvocationHandler tih) {
-		return (Tuple2<T1, T2>) Proxy.newProxyInstance(Tup.class.getClassLoader(), new Class[] { Tuple2.class}, tih);
+	private static <T> T createProxy(Class<? super T> tupleClass, Object... tupleArgs) {
+		TupleInvocationHandler tih = new TupleInvocationHandler(tupleArgs);
+		return (T) Proxy.newProxyInstance(Tup.class.getClassLoader(), new Class[] { tupleClass }, tih);
 	}
 	
 	@SuppressWarnings("unchecked")
-	private static <T1, T2, T3> Tuple3<T1, T2, T3> createProxy3(TupleInvocationHandler tih) {
-		return (Tuple3<T1, T2, T3>) Proxy.newProxyInstance(Tup.class.getClassLoader(), new Class[] { Tuple3.class}, tih);
+	private static <T> T createProxy(Class<? super T> tupleClass, int tupleSize) {
+		TupleInvocationHandler tih = new TupleInvocationHandler(tupleSize);
+		return (T) Proxy.newProxyInstance(Tup.class.getClassLoader(), new Class[] { tupleClass }, tih);
 	}
 }
